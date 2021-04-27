2d738f6bd0ff3ef0feed17b05271d873cc0378ee
Spector <arshkumar9@live.com>
2021-04-27 18:53:14 +0530

    Added missing programs

--- install_script.sh
+++ install_script.sh
@@ -3,8 +3,9 @@
 # A script to install all the packages (For Arch/Arch-based distros)
 if [ $1 = "install" ] ; then
     sudo pacman -Syu 
-    sudo pacman -S bspwm sxhkd polybar picom stalonetray dunst nitrogen alacritty network-manager-applet blueman rofi maim pulseaudio pasystray nm-connection-editor \
-    thunar xfce4-power-manager yay lxappearance parcellite xclip zathura neovim udiskie zsh stow thunar-volman polkit-gnome
+    sudo pacman -S bspwm sxhkd polybar picom stalonetray dunst nitrogen alacritty networkmanager network-manager-applet blueman rofi maim pulseaudio pasystray nm-connection-editor \
+    thunar xfce4-power-manager yay lxappearance parcellite xclip zathura neovim udiskie zsh stow thunar-volman polkit-gnome capitaine-cursors noto-fonts ttf-roboto adapta-gtk-theme \
+    nerd-fonts-noto-sans-mono xorg mpv pavucontrol alsa-lib alsa-plugins alsa-utils brightnessctl mpv htop
 fi
 
 
