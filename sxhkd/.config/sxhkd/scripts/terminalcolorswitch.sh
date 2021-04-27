2d27de9825954e6766ea65a8229c8cf56c046b73
Spector <arshkumar9@live.com>
2021-04-27 18:49:52 +0530

    Fixed the scripts and added terminal color change script

---
+++ sxhkd/.config/sxhkd/scripts/terminalcolorswitch.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+name="$(grep \& .config/alacritty/alacritty.yml | sed "s/^ *//g" | cut -d: -f1 | fzf)"
+([ ! "$name" = '' ] && sed -i "s/^colors:.*/colors: \*$name/g" "$HOME"/.config/alacritty/alacritty.yml) | exit 0
