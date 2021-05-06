173cefe3bf0730768c35e855dd14854a7951e667
Spector <arshkumar9@live.com>
2021-05-06 11:47:37 +0530

    Added more option in install script

--- install_script.sh
+++ install_script.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
 # A script to install all the packages (For Arch/Arch-based distros)
-if [ $1 = "install" ] ; then
+if [ "$1" = "install" ] ; then
     sudo pacman -Syu 
-    sudo pacman -S bspwm sxhkd polybar picom stalonetray dunst nitrogen alacritty networkmanager network-manager-applet blueman rofi maim pulseaudio pasystray nm-connection-editor \
-    thunar xfce4-power-manager yay lxappearance parcellite xclip zathura neovim udiskie zsh stow thunar-volman polkit-gnome capitaine-cursors noto-fonts ttf-roboto adapta-gtk-theme \
-    nerd-fonts-noto-sans-mono xorg mpv pavucontrol alsa-lib alsa-plugins alsa-utils brightnessctl mpv htop
+    sudo pacman -S $(<$(pwd)/extras/package)
 fi
 
 
 # enabling service
-if [ $1 = "service" ] ; then
+if [ "$1" = "service" ] ; then
     sudo systemctl enable NetworkManager.service
 fi
 
 # Sym-linking dot-files
-if [ $1 = "link" ] ; then
-    stow -v $(ls -A -I "README.md" -I "keyboard and mouse config" -I ".git*" -I "install_script.sh")
+if [ "$1" = "link" ] ; then
+    stow -v "$(ls -A -I "README.md" -I "extras" -I ".git*" -I "install_script.sh")"
 fi
 
+[ "$1" = "copy" ] && printf "Copying the environment var file.\n" && sudo cat "$(pwd)/extras/etc/environment" >> /etc/environment \
+                  && printf "Copying keyboard, touchpad & monitor configs.\n" && sudo cp -vi  "$(pwd)/extras/etc/X11/xorg.conf.d/*" /etc/X11/xorg.conf.d/
+
+[ $# -lt 1 ] && printf "Basic options:\ninstall - Install the necessary applications
+service - Enables the required services.
+copy - Copies the env var file and xorg keyboard, touchpad and monitor configs.
+link - Sets up the dotfiles by linking them.\n"
